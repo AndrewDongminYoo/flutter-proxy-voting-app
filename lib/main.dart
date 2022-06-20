c1af9b8fcb3e32cf35f73afffb907426bac9d88f
Dongmin,Yoo <ydm2790@gmail.com>
2022-06-20 21:04:53 +0900

    ➕ Add firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart'
     show Get, GetMaterialApp, GetNavigation, Transition;
-// import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
+import 'utils/firebase.dart';
+import 'firebase_options.dart';
 import 'routes.dart' show routes;
 
-void main() {
-  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  // after initialize
-  // FlutterNativeSplash.remove();
+void main() async {
+  // Keep splash screen
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  // initialize app
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  // Remove splash screen
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
@@ -28,6 +37,8 @@ class MyApp extends StatelessWidget {
       defaultTransition: Transition.cupertino,
       initialRoute: '/onboarding',
       getPages: routes(),
+      navigatorKey: Get.key,
+      navigatorObservers: [FirebaseAnalyticsObserver(analytics: analytics)],
     );
   }
 }
