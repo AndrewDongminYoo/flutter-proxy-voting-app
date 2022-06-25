0fca8b95485d3c79f6f0fd49cfb6c6fab7478305
Dongmin,Yoo <ydm2790@gmail.com>
2022-06-25 15:28:12 +0900

    💄 more clickable in home

--- lib/campaign/campaign_info.dart
+++ lib/campaign/campaign_info.dart
@@ -4,29 +4,38 @@ import 'campaign.model.dart';
 
 class CampaignInfo extends StatelessWidget {
   const CampaignInfo(
-      {Key? key,
-      required this.campaign})
+      {Key? key, required this.campaign, required this.onPress})
       : super(key: key);
   final Campaign campaign;
+  final Function(Campaign) onPress;
 
   @override
   Widget build(BuildContext context) {
     return Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
       const SizedBox(height: 24),
-      Text(
-        campaign.companyName,
-        style: const TextStyle(
-            color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold),
+      GestureDetector(
+        onTap: () => {onPress(campaign)},
+        child: Text(
+          campaign.companyName,
+          style: const TextStyle(
+              color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold),
+        ),
       ),
       const SizedBox(height: 16),
-      Text(
-        campaign.moderator,
-        style: const TextStyle(color: Colors.white, fontSize: 16, height: 1),
+      GestureDetector(
+        onTap: () => {onPress(campaign)},
+        child: Text(
+          campaign.moderator,
+          style: const TextStyle(color: Colors.white, fontSize: 16, height: 1),
+        ),
       ),
       const SizedBox(height: 16),
-      Text(
-        campaign.date,
-        style: const TextStyle(color: Colors.white, fontSize: 16),
+      GestureDetector(
+        onTap: () => {onPress(campaign)},
+        child: Text(
+          campaign.date,
+          style: const TextStyle(color: Colors.white, fontSize: 16),
+        ),
       ),
       const SizedBox(height: 16),
     ]);
