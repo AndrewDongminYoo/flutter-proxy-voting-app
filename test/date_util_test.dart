a564c8d400d86f461c14ab3a7f29abd63bcf53c2
Dongmin,Yoo <ydm2790@gmail.com>
2022-07-26 17:28:49 +0900

    ✅ Add datetuil unit test

---
+++ test/date_util_test.dart
@@ -0,0 +1,34 @@
+// 📦 Package imports:
+import 'package:flutter_test/flutter_test.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+final now = DateTime.now();
+
+void main() {
+  group('Counter', () {
+    timeago.setLocaleMessages('ko', timeago.KoMessages());
+    test('a moment ago', () {
+      final clock = now.add(const Duration(seconds: 1));
+      var result = timeago.format(now, clock: clock, locale: 'ko');
+      expect(result, equals('방금 전'));
+    });
+
+    test('2 minutes ago', () {
+      final clock = now.add(const Duration(minutes: 2));
+      var result = timeago.format(now, clock: clock, locale: 'ko');
+      expect(result, equals('2 분 전'));
+    });
+
+    test('1 hour ago', () {
+      final clock = now.add(const Duration(minutes: 70));
+      var result = timeago.format(now, clock: clock, locale: 'ko');
+      expect(result, equals('약 1시간 전'));
+    });
+
+    test('2 days ago', () {
+      final clock = now.add(const Duration(days: 2, minutes: 2));
+      var result = timeago.format(now, clock: clock, locale: 'ko');
+      expect(result, equals('2일 전'));
+    });
+  });
+}
