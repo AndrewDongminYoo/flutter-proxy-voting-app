da978690d359d8e7d1db7727a8ff2f0ff476fd38
Dongmin,Yoo <ydm2790@gmail.com>
2022-08-22 10:21:06 +0900

    🔥 delete empty file

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -3,7 +3,6 @@ export 'date_util.dart';
 export 'exceptions.dart';
 export 'firebase.dart';
 export 'firebase_options.dart';
-export 'html/html.dart';
 export 'info_plus/info_plus.dart';
 export 'mailto.dart';
 export 'shared_prefs.dart';
