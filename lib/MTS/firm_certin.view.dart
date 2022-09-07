30328e6835a6346b90562e7cad649183e74d5deb
Dongmin,Yoo <ydm2790@gmail.com>
2022-09-07 09:36:14 +0900

    🔥SASEnvironment.setString

--- lib/MTS/firm_certin.view.dart
+++ lib/MTS/firm_certin.view.dart
@@ -69,102 +69,100 @@ class _MTSLoginCertPageState extends State<MTSLoginCertPage> {
   }
 
   Column loginWithCertification() {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      CustomText(
-        text: '공동인증서 선택',
-        typoType: TypoType.h1Title,
-        textAlign: TextAlign.start,
-        isFullWidth: true,
-      ),
-      CustomText(
-        text:
-            '증권사 연동을 위해 공동인증서를 선택해주세요.\n일부 증권사의 경우 공동인증서와 증권사 ID를\n모두 요구할 수도 있습니다.',
-        typoType: TypoType.body,
-        textAlign: TextAlign.start,
-        isFullWidth: true,
-      ),
-      Container(
-        width: Get.width,
-        alignment: Alignment.center,
-        margin: const EdgeInsets.symmetric(vertical: 20),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: certificationList
-              .map(
-                (cert) => CertificationCard(
-                  item: cert,
-                  onPressed: () {
-                    _mtsController.setCertification(cert);
-                    setState(() {
-                      certificationList.removeWhere((e) => e != cert);
-                      showInputElement = true;
-                    });
-                  },
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomText(
+          text: '공동인증서 선택',
+          typoType: TypoType.h1Title,
+          textAlign: TextAlign.start,
+          isFullWidth: true,
+        ),
+        CustomText(
+          text:
+              '증권사 연동을 위해 공동인증서를 선택해주세요.\n일부 증권사의 경우 공동인증서와 증권사 ID를\n모두 요구할 수도 있습니다.',
+          typoType: TypoType.body,
+          textAlign: TextAlign.start,
+          isFullWidth: true,
+        ),
+        Container(
+            width: Get.width,
+            alignment: Alignment.center,
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: certificationList
+                    .map((cert) => CertificationCard(
+                        item: cert,
+                        onPressed: () {
+                          _mtsController.setCertification(cert);
+                          setState(() {
+                            certificationList.removeWhere((e) => e != cert);
+                            showInputElement = true;
+                          });
+                        }))
+                    .toList())),
+        showInputElement
+            ? TextFormField(
+                autofocus: true,
+                initialValue: _certId,
+                onChanged: (val) => {
+                  setState(() {
+                    _certId = val;
+                  })
+                },
+                style: authFormFieldStyle,
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '증권사 아이디',
+                  helperText: '아이디를 입력해주세요',
                 ),
               )
-              .toList(),
-        ),
-      ),
-      showInputElement
-          ? TextFormField(
-              autofocus: true,
-              initialValue: _certId,
-              onChanged: (val) => {
-                setState(() {
-                  _certId = val;
-                })
-              },
-              style: authFormFieldStyle,
-              keyboardType: TextInputType.name,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '증권사 아이디',
-                helperText: '아이디를 입력해주세요',
-              ),
-            )
-          : Container(),
-      showInputElement
-          ? TextFormField(
-              initialValue: _certPW,
-              onChanged: (val) => {
-                setState(() {
-                  _certPW = val;
-                })
-              },
-              autofocus: true,
-              style: authFormFieldStyle,
-              obscureText: !passwordVisible,
-              keyboardType: passwordVisible
-                  ? TextInputType.visiblePassword
-                  : TextInputType.text,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                labelText: '인증서 비밀번호',
-                helperText: '비밀번호를 입력해주세요',
-                suffixIcon: InkWell(
-                  onTap: () => setState(() {
-                    _setVisible(!passwordVisible);
-                  }),
-                  child: passwordVisible
-                      ? const Icon(Icons.remove_red_eye_outlined)
-                      : const Icon(Icons.remove_red_eye),
+            : Container(),
+        showInputElement
+            ? TextFormField(
+                initialValue: _certPW,
+                onChanged: (val) => {
+                  setState(() {
+                    _certPW = val;
+                  })
+                },
+                autofocus: true,
+                style: authFormFieldStyle,
+                obscureText: !passwordVisible,
+                keyboardType: passwordVisible
+                    ? TextInputType.visiblePassword
+                    : TextInputType.text,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: '인증서 비밀번호',
+                  helperText: '비밀번호를 입력해주세요',
+                  suffixIcon: InkWell(
+                    onTap: () => setState(() {
+                      _setVisible(!passwordVisible);
+                    }),
+                    child: passwordVisible
+                        ? const Icon(Icons.remove_red_eye_outlined)
+                        : const Icon(Icons.remove_red_eye),
+                  ),
                 ),
+                onFieldSubmitted: (password) async => {
+                  _mtsController.setCertPW(password),
+                  _mtsController.setCertID(_certId),
+                  await _mtsController.showMTSResult(),
+                },
+              )
+            : CustomButton(
+                onPressed: () {
+                  certificationList.clear();
+                  _mtsController.emptyCerts();
+                  goMTSImportCert();
+                },
+                label: '공동인증서 삭제',
               ),
-              onFieldSubmitted: (password) async => {
-                _mtsController.setCertPW(password),
-                _mtsController.setCertID(_certId),
-                await _mtsController.showMTSResult(),
-              },
-            )
-          : CustomButton(
-              onPressed: () {
-                certificationList.clear();
-                _mtsController.emptyCerts();
-                goMTSLoginCert();
-              },
-              label: '공동인증서 삭제',
-            ),
-    ]);
+      ],
+    );
   }
 }
